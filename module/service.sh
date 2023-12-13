d2e86d5c65e5377a016c0f84c512e3e15f15dd79
Yiğit <40565628+tryigit@users.noreply.github.com>
2023-12-13 11:46:57 +0300

    Update service.sh
    Removing Magisk 32bit support and making the code structure more logical. If it does not work on some special devices and you want it to work, add the prop changes in the previous file yourself. This is good for me.

--- module/service.sh
+++ module/service.sh
@@ -1,53 +1,51 @@
-# Conditional sensitive properties
+#!/system/bin/sh
 
-resetprop_if_diff() {
-    local NAME=$1
-    local EXPECTED=$2
-    local CURRENT=$(resetprop $NAME)
-
-    [ -z "$CURRENT" ] || [ "$CURRENT" == "$EXPECTED" ] || resetprop $NAME $EXPECTED
-}
-resetprop_if_match() {
-    local NAME=$1
-    local CONTAINS=$2
-    local VALUE=$3
-
-    [[ "$(resetprop $NAME)" == *"$CONTAINS"* ]] && resetprop $NAME $VALUE
+check_reset_prop() {
+  local NAME=$1
+  local EXPECTED=$2
+  local VALUE=$(resetprop $NAME)
+  [ -z $VALUE ] || [ $VALUE = $EXPECTED ] || resetprop $NAME $EXPECTED
 }
 
-# Magisk recovery mode
-resetprop_if_match ro.bootmode recovery unknown
-resetprop_if_match ro.boot.mode recovery unknown
-resetprop_if_match vendor.boot.mode recovery unknown
+contains_reset_prop() {
+  local NAME=$1
+  local CONTAINS=$2
+  local NEWVAL=$3
+  [[ "$(resetprop $NAME)" = *"$CONTAINS"* ]] && resetprop $NAME $NEWVAL
+}
 
-# SELinux
-if [ -n "$(resetprop ro.build.selinux)" ]; then
-    resetprop --delete ro.build.selinux
-fi
-# use toybox to protect *stat* access time reading
-if [ "$(toybox cat /sys/fs/selinux/enforce)" == "0" ]; then
-    chmod 640 /sys/fs/selinux/enforce
-    chmod 440 /sys/fs/selinux/policy
-fi
-
-# SafetyNet/Play Integrity
 {
-    # late props which must be set after boot_completed for various OEMs
-    until [ "$(getprop sys.boot_completed)" == "1" ]; do
-        sleep 1
-    done
+  # Reset props after boot completed to avoid breaking some weird devices/ROMs...
+  while [ "$(getprop sys.boot_completed)" != "1" ]
+  do
+    sleep 1
+  done
 
-    # Avoid breaking Realme fingerprint scanners
-    resetprop_if_diff ro.boot.flash.locked 1
+  check_reset_prop "ro.boot.vbmeta.device_state" "locked"
+  check_reset_prop "ro.boot.verifiedbootstate" "green"
+  check_reset_prop "ro.boot.flash.locked" "1"
+  check_reset_prop "ro.boot.veritymode" "enforcing"
+  check_reset_prop "ro.boot.warranty_bit" "0"
+  check_reset_prop "ro.warranty_bit" "0"
+  check_reset_prop "ro.debuggable" "0"
+  check_reset_prop "ro.secure" "1"
+  check_reset_prop "ro.adb.secure" "1"
+  check_reset_prop "ro.build.type" "user"
+  check_reset_prop "ro.build.tags" "release-keys"
+  check_reset_prop "ro.vendor.boot.warranty_bit" "0"
+  check_reset_prop "ro.vendor.warranty_bit" "0"
+  check_reset_prop "vendor.boot.vbmeta.device_state" "locked"
+  check_reset_prop "vendor.boot.verifiedbootstate" "green"
+  check_reset_prop "ro.secureboot.lockstate" "locked"
 
-    # Avoid breaking Oppo fingerprint scanners
-    resetprop_if_diff ro.boot.vbmeta.device_state locked
+  # Hide that we booted from recovery when magisk is in recovery mode
+  contains_reset_prop "ro.bootmode" "recovery" "unknown"
+  contains_reset_prop "ro.boot.bootmode" "recovery" "unknown"
+  contains_reset_prop "vendor.boot.bootmode" "recovery" "unknown"
 
-    # Avoid breaking OnePlus display modes/fingerprint scanners
-    resetprop_if_diff vendor.boot.verifiedbootstate green
-
-    # Avoid breaking OnePlus/Oppo display fingerprint scanners on OOS/ColorOS 12+
-    resetprop_if_diff ro.boot.verifiedbootstate green
-    resetprop_if_diff ro.boot.veritymode enforcing
-    resetprop_if_diff vendor.boot.vbmeta.device_state locked
+  resetprop --delete ro.build.selinux
 }&
+
+# Always remove Magisk 32bit support
+rm -f /debug_ramdisk/magisk32
+killall -9 zygiskd32
