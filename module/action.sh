c27dd0ae2c0755a40a58e3ce96f6fcd9839621d7
Rev4N1 <benjaminruizmonti@gmail.com>
2024-10-29 10:14:18 +0100

    action.sh: dialog timeout on KernelSU

--- module/action.sh
+++ module/action.sh
@@ -9,7 +9,7 @@ sh $MODPATH/autopif2.sh || exit 1
 echo -e "\nDone!"
 
 # warn since Magisk's implementation automatically closes if successful
-if [ "$KSU" != "true" -a "$APATCH" != "true" ]; then
+if [ "$APATCH" != "true" ]; then
     echo -e "\nClosing dialog in 20 seconds ..."
     sleep 20
 fi
