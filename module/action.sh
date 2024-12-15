229b7010cc812613d7ded9fcfdff961a4929047a
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-12-15 17:44:02 +0800

    module/action: let tricky_store do its job
    4 things has to pass at the very least
    the module exists
    it is not disabled
    theres a keybox
    gms is on target
    
    if all good, we can probably give ts a chance

--- module/action.sh
+++ module/action.sh
@@ -4,7 +4,14 @@ MODPATH="${0%/*}"
 set +o standalone
 unset ASH_STANDALONE
 
-sh $MODPATH/autopif2.sh || exit 1
+if [ -d /data/adb/modules/tricky_store ] && [ ! -f /data/adb/modules/tricky_store/disable ] &&
+	[ -f /data/adb/tricky_store/keybox.xml ] && grep -q "com.google.android.gms" /data/adb/tricky_store/target.txt > /dev/null ; then
+	# all these passes, then we can
+	# let tricky store do its job
+	sh $MODPATH/autopif2.sh --strong || exit 1
+else
+	sh $MODPATH/autopif2.sh || exit 1
+fi
 
 echo "Done!"
 
