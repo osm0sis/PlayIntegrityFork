0940673dd6589504169788fefc897c1d5e376322
backslashxx <118538522+backslashxx@users.noreply.github.com>
2024-12-15 16:55:06 +0800

    module/action: don't sleep on MMRL action

--- module/action.sh
+++ module/action.sh
@@ -8,8 +8,10 @@ sh $MODPATH/autopif2.sh || exit 1
 
 echo -e "\nDone!"
 
-# warn since KernelSU's implementation automatically closes if successful
-if [ "$KSU" = "true" ]; then
+# dont sleep on Magisk and MMRL
+# those environments stops exec
+# and lets the user read
+if [ -z "$MAGISKTMP" ] && [ -z "$MMRL" ]; then
     echo -e "\nClosing dialog in 20 seconds ..."
     sleep 20
 fi
