42dae73f06f6d534aa168b628ee0c5af062ef86a
gavdoc38 <gavin.a.docherty@gmail.com>
2025-08-29 00:10:40 +0100

    Update main.cpp

--- app/src/main/cpp/main.cpp
+++ app/src/main/cpp/main.cpp
@@ -313,7 +313,7 @@ private:
 
         if (pkgName == VENDING_PACKAGE) {
             LOGD("JNI %s: Calling EntryPointVending.init", niceName);
-            auto entryInit = env->GetStaticMethodID(entryClass, "init", "(II)V");
+            auto entryInit = env->GetStaticMethodID(entryClass, "init", "(IIILjava/lang/String;)V");
             env->CallStaticVoidMethod(entryClass, entryInit, verboseLogs, spoofVendingFingerprint, spoofVendingSdk, spoofFingerprintValue.c_str());
         } else {
             LOGD("JNI %s: Sending JSON", niceName);
