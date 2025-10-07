c300718b7b805a3f4abbfe4fb3055a64fde28c0f
Chris Renshaw <osm0sis@outlook.com>
2025-10-06 22:36:14 -0300

    Update SDK Build Tools

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ android {
     namespace = "es.chiteroman.playintegrityfix"
     compileSdk = 36
     ndkVersion = "28.2.13676358"
-    buildToolsVersion = "36.0.0"
+    buildToolsVersion = "36.1.0"
 
     buildFeatures {
         prefab = true
