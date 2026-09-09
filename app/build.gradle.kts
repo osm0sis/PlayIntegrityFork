4defbf811384d9e4af241b50841139bc757ae3c0
osm0sis <osm0sis@outlook.com>
2026-09-09 10:17:09 -0300

    Update NDK

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 android {
     namespace = "es.chiteroman.playintegrityfix"
     compileSdk = 37
-    ndkVersion = "29.0.14206865"
+    ndkVersion = "30.0.16248370"
     buildToolsVersion = "37.0.0"
 
     buildFeatures {
